f364f9d71a661782ce9064aeaaf436920dcc6fad
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-02-21 00:46:40 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import { Button } from '@chakra-ui/react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import ChatPage from './pages/ChatPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
